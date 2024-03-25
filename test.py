b7b4bb7360014ad832d12f92e835e07500c5cbb1
kartik-225 <156411717+kartik-225@users.noreply.github.com>
2024-03-25 15:39:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("Worked Kartik!")
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.query import Query
+from urllib.parse import parse_qs
+import os
+
+html = '''<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Contact Form</title>
+  </head>
+  <body>
+    <form action="/" method="POST">
+      <input type="text" id="name" name="name" placeholder="Name" required>
+      <input type="email" id="email" name="email" placeholder="Email" required>
+      <textarea id="content" name="content" placeholder="Message" required></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
+'''
+
+def main(context):
+    if context.req.method == 'GET':
+        return context.res.send(html, 200, {'content-type': 'text/html'})
+    return context.res.send('Not found', 404)
